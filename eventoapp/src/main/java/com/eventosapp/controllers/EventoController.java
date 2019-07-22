d8ee1923b0e5210f16c97c9ede46a1ef7a5840d8
Gabrielli Correa <gabriellicorrea7@gmail.com>
2019-07-22 09:25:43 -0300

    Salvando dados no banco.

--- eventoapp/src/main/java/com/eventosapp/controllers/EventoController.java
+++ eventoapp/src/main/java/com/eventosapp/controllers/EventoController.java
@@ -1,13 +1,28 @@
 package com.eventosapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eventosapp.models.Evento;
+import com.eventosapp.repository.EventoRepository;
 
 @Controller
 public class EventoController {
+	@Autowired
+	private EventoRepository er;
 	
-	@RequestMapping("/cadastrarEvento")
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
 	private String form() {
 		return "evento/formEvento";
+	}
+	
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.POST)
+	private String form(Evento evento) {
+		
+		er.save(evento);
+		
+		return "redirect:/cadastrarEvento";
 	}
 }
