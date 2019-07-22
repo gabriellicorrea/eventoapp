7e0317688fea8ab52673669a0f6d4ac9daa21d3f
Gabrielli Correa <gabriellicorrea7@gmail.com>
2019-07-22 09:45:46 -0300

    Listagem de eventos

--- eventoapp/src/main/java/com/eventosapp/controllers/EventoController.java
+++ eventoapp/src/main/java/com/eventosapp/controllers/EventoController.java
@@ -2,8 +2,10 @@ package com.eventosapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eventosapp.models.Evento;
 import com.eventosapp.repository.EventoRepository;
@@ -25,4 +27,13 @@ public class EventoController {
 		
 		return "redirect:/cadastrarEvento";
 	}
+
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index.html");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos );
+		return mv;
+	}
+
 }
