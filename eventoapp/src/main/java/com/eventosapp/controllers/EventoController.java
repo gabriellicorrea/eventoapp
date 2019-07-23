58b1c84c1d750ad4f44e6f57d42d885806f67b79
Gabrielli Correa <gabriellicorrea7@gmail.com>
2019-07-23 11:10:26 -0300

    Cadastrando convidados no Banco

--- eventoapp/src/main/java/com/eventosapp/controllers/EventoController.java
+++ eventoapp/src/main/java/com/eventosapp/controllers/EventoController.java
@@ -7,13 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.eventosapp.models.Convidado;
 import com.eventosapp.models.Evento;
+import com.eventosapp.repository.ConvidadoRepository;
 import com.eventosapp.repository.EventoRepository;
 
 @Controller
 public class EventoController {
 	@Autowired
 	private EventoRepository er;
+	
+	@Autowired
+	private ConvidadoRepository cr;
 	
 	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
 	private String form() {
@@ -36,7 +41,7 @@ public class EventoController {
 		return mv;
 	}
 	
-	@RequestMapping("/{codigo}")
+	@RequestMapping(value="/{codigo}", method=RequestMethod.GET)
 	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) {
 		Evento evento = er.findByCodigo(codigo);
 		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
@@ -44,4 +49,13 @@ public class EventoController {
 		return mv;
 	}
 
+	@RequestMapping(value="/{codigo}", method=RequestMethod.POST)
+	public String detalhesEventoPost(@PathVariable("codigo") long codigo, Convidado convidado){
+		Evento evento = er.findByCodigo(codigo);
+		convidado.setEvento(evento);
+		cr.save(convidado);
+		return "redirect:/{codigo}";
+	}
+	
+	
 }
